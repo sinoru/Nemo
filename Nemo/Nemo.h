3e779da3d9adfe854af7decc8151ac146959bab9
Jaehong Kang <sinoru@me.com>
2015-08-08 21:06:52 +0900

    Initial project

---
+++ Nemo/Nemo.h
@@ -0,0 +1,19 @@
+//
+//  Nemo.h
+//  Nemo
+//
+//  Created by Sinoru on 2015. 8. 8..
+//  Copyright (c) 2015년 Sinoru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nemo.
+FOUNDATION_EXPORT double NemoVersionNumber;
+
+//! Project version string for Nemo.
+FOUNDATION_EXPORT const unsigned char NemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nemo/PublicHeader.h>
+
+
