0c08cc796d285cea80f2bfe2ae60a3ea9d93ac7f
Jaehong Kang <sinoru@me.com>
2015-08-10 17:19:57 +0900

    Lots of UI tweaks

--- Nemo/RecentPhotosPhotoCollectionViewCell.swift
+++ Nemo/RecentPhotosPhotoCollectionViewCell.swift
@@ -15,10 +15,27 @@ class CheckIndicatorView: UIView {
         }
     }
     
-    @IBOutlet dynamic weak private var checkMark: UILabel!
+    weak private var checkMark: UILabel!
     
     override init(frame: CGRect) {
+        let checkMark = UILabel()
+        
+        self.checkMark = checkMark
+        
         super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.clearColor()
+        
+        self.checkMark.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.checkMark.text = "✓"
+        self.checkMark.font = UIFont.systemFontOfSize(20)
+        self.checkMark.textColor = UIColor.whiteColor()
+        self.checkMark.textAlignment = .Center
+        
+        self.addSubview(checkMark)
+        
+        self.addConstraint(NSLayoutConstraint(item: self, attribute: .CenterX, relatedBy: .Equal, toItem: self.checkMark, attribute: .CenterX, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: self, attribute: .CenterY, relatedBy: .Equal, toItem: self.checkMark, attribute: .CenterY, multiplier: 1.0, constant: -1.0))
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -59,7 +76,7 @@ class RecentPhotosPhotoCollectionViewCell: UICollectionViewCell {
         }
     }
     
-    dynamic weak private var imageView: UIImageView!
+    weak private var imageView: UIImageView!
     weak var checkIndicatorView: CheckIndicatorView!
     
     override init(frame: CGRect) {
@@ -71,13 +88,27 @@ class RecentPhotosPhotoCollectionViewCell: UICollectionViewCell {
         
         super.init(frame: frame)
         
-        self.addSubview(imageView)
-        self.addSubview(checkIndicatorView)
-        
+        self.imageView.contentMode = .ScaleAspectFill
         self.imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
         
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[imageView]-|", options: nil, metrics: nil, views: ["imageView": imageView]))
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[imageView]-|", options: nil, metrics: nil, views: ["imageView": imageView]))
+        self.checkIndicatorView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        self.contentView.addSubview(imageView)
+        self.contentView.addSubview(checkIndicatorView)
+        
+        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[imageView]-0-|", options: nil, metrics: nil, views: ["imageView": imageView]))
+        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[imageView]-0-|", options: nil, metrics: nil, views: ["imageView": imageView]))
+        
+        self.contentView.addConstraint(NSLayoutConstraint(item: checkIndicatorView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30.0))
+        self.contentView.addConstraint(NSLayoutConstraint(item: checkIndicatorView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30.0))
+        
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.contentView, attribute: .Leading, relatedBy: .LessThanOrEqual, toItem: checkIndicatorView, attribute: .Leading, multiplier: 1.0, constant: -4.0))
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.contentView, attribute: .Trailing, relatedBy: .GreaterThanOrEqual, toItem: checkIndicatorView, attribute: .Trailing, multiplier: 1.0, constant: 4.0))
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.contentView, attribute: .Bottom, relatedBy: .Equal, toItem: checkIndicatorView, attribute: .Bottom, multiplier: 1.0, constant: 4.0))
+        
+        let trailingEqualConstraint = NSLayoutConstraint(item: self.contentView, attribute: .Trailing, relatedBy: .Equal, toItem: checkIndicatorView, attribute: .Trailing, multiplier: 1.0, constant: 4.0)
+        trailingEqualConstraint.priority = 500
+        self.contentView.addConstraint(trailingEqualConstraint)
     }
     
     required init(coder aDecoder: NSCoder) {
